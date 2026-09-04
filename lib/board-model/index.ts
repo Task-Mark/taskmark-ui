627b49448d656d5951d780acda2aa1c682c3316a
Marco Mendão <marco.mendao@betacode.tech>
2026-09-04 16:29:59 +0100

    export worklog helpers for webpack

--- lib/board-model/index.ts
+++ lib/board-model/index.ts
@@ -1,26 +1,20 @@
-export {
-  SNAPSHOT_PATH,
-  storyKey,
-  type BoardSnapshot,
-} from "./snapshot-types"
+export { SNAPSHOT_PATH, storyKey } from "./snapshot-types"
+export type { BoardSnapshot } from "./snapshot-types"
 export type { DiscoveredProject } from "./types"
 export type { WorkItemDetail, WorkItemRef } from "./detail-types"
-export {
-  WORK_ACTIVITY_MAX_VISIBLE,
-  type WorkActivityEvent,
-  type WorkPresenceCard,
-} from "./work-activity"
-export {
-  flattenWorklogEntries,
-  type WorklogEntry,
-} from "./worklog"
+export { WORK_ACTIVITY_MAX_VISIBLE } from "./work-activity"
+export type { WorkActivityEvent, WorkPresenceCard } from "./work-activity"
+export { flattenWorklogEntries } from "./worklog"
+export type { WorklogEntry } from "./worklog"
 export {
   WORKLOG_PACE_HISTORY_DAYS,
   WORKLOG_PACE_LOOKBACK_DAYS,
   dailyWorklogHistory,
   dailyWorklogPace,
   worklogPaceIcon,
-  type WorklogPace,
-  type WorklogPaceDay,
-  type WorklogPaceIcon,
+} from "./worklog-pace"
+export type {
+  WorklogPace,
+  WorklogPaceDay,
+  WorklogPaceIcon,
 } from "./worklog-pace"
